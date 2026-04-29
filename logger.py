41753d747ffa9407e9bf6ffaf5e4fc921716ab60
Owen Brown <owen@Owens-Mac-mini.local>
2026-04-29 18:33:50 -0500

    logging

---
+++ logger.py
@@ -0,0 +1,14 @@
+import logging, sys
+
+LOG_FORMAT = "%(asctime)s | %(levelname)-8s | %(name)s:%(funcName)s:%(lineno)d - %(message)s"
+
+def setup_logging():
+    stream_handler = logging.StreamHandler(sys.stdout)
+    stream_handler.setFormatter(logging.Formatter(LOG_FORMAT))
+    root_logger = logging.getLogger()
+    if root_logger.hasHandlers():
+        root_logger.handlers.clear()
+    root_logger.setLevel(logging.INFO)
+    root_logger.addHandler(stream_handler)
+    logging.getLogger("httpx").setLevel(logging.WARNING)
+    logging.getLogger("sqlalchemy.engine").setLevel(logging.WARNING)
