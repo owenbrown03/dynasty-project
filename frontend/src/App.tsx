50bb224b637f7a089787d5c58d85f8deb12ff1f9
Owen Brown <owen@Owens-Mac-mini.local>
2026-05-12 16:42:21 -0500

    feat: Trade signals

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,53 +1,26 @@
-import { useState } from 'react';
-import api from './api/client.ts';
-import type { Transaction } from './types/index.ts';
-import TradeCards from './components/TradeCards';
-import UsernameInput from './components/UsernameInput';
+import AppRoutes from './Routes';
+import Sidebar from './components/Sidebar';
+import usernameLookup from './hooks/usernameLookup.ts';
 
 function App() {
-  const [transactions, setTransactions] = useState<Transaction[]>([]);
-  const [username, setUsername] = useState('');
-  const [loading, setLoading] = useState<boolean>(false);
-
-  const handleUserSubmit = async (name: string) => {
-    if (!name || name === username) return;
-
-    setLoading(true);
-    setUsername(name); 
-
-    try {
-      const res = await api.post<Transaction[]>(`/users/${name}/sync`);
-      setTransactions(res.data);
-    } 
-    catch (err) {
-      console.error("Fetch failed", err);
-    } 
-    finally {
-      setLoading(false);
-    }
-  }; 
+  const { transactions, username, loading, handleUserSubmit } = usernameLookup(); 
 
   return (
     <div className="app-container">
-      <h1>
-        Dynasty Trade Signals
-      </h1>
-      <UsernameInput 
-        setUsername={handleUserSubmit} 
-      />
-      {
-        loading && <p>Fetching data from Sleeper...</p>
-      }
-      {!loading && transactions.length > 0 && (
-        <TradeCards 
-          transactions={transactions} 
+      <nav className="sidebar-container">
+        <Sidebar />
+      </nav>
+      
+      <main>
+        <AppRoutes 
+          handleUserSubmit={handleUserSubmit}
+          transactions={transactions}
+          loading={loading}
+          username={username}
         />
-      )}
-      {!loading && username && transactions.length === 0 && (
-        <p>No transactions found for {username}.</p>
-      )}
+      </main>
     </div>
   );
-}
 
+}
 export default App;
